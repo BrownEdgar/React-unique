0503f4b3e39e40019add6c613d24ac1ea2c06c31
Sash-1 <sasha.aristakesyan@mail.ru>
2023-06-04 18:10:11 +0400

    todos

--- src/users/Sash/App.jsx
+++ src/users/Sash/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import { useState, useEffect } from 'react'
+import "./App.css"
 export default function App() {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		const getData = () => {
+			fetch('https://jsonplaceholder.typicode.com/todos')
+				.then(res => res.json())
+				.then(json => setData(json))
+		}
+		getData()
+	},[])
+	const deleteBox = (id)=>{
+		const filtered = data.filter(elem => elem.id != id)
+		setData(filtered)
+		 }
+
 	return (
-		<div>
-			<p>Lorem ipsum dolor sit amet.</p>
+		<div className='box'>
+			{
+				data.map(elem => <div key={elem.id} className='text'>
+				<h2> <span>Title:</span>{elem.title} <span className='del' onClick={()=>deleteBox(elem.id)}>&#10060;</span></h2>
+				<p> <span>UserId:</span>{elem.userId}</p>
+				<p> <span>Completed:</span>{elem.completed}</p>
+				</div>)
+			}
 		</div>
 	)
 }
