e154f223c9c4cb35531f9dc8491f369ba2f9aeff
ArmenDashysn <dashyan.armen@gmail.com>
2023-06-07 00:27:26 +0400

    add card

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './users/Edgar/Fetch/App'
+import App from './users/Armen/worckwithImages/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
