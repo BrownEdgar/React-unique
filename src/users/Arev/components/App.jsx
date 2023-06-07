f50c2bba451f7fe507c059b4fd17e80c1b3e7c44
avetisyan19 <ghukasmnatsakanyan@gmail.com>
2023-06-07 15:31:15 +0300

    Travel, example one

---
+++ src/users/Arev/components/App.jsx
@@ -0,0 +1,18 @@
+// import {useState} from 'react'
+// import Card from './Card/Card'
+
+// export default function App() {
+//     const [data, setdata] = useState({
+//         section:"Travel",
+//         title:"10 Best Things to Do in Seattle",
+//         description:"Seattle is a seaport city on the west coast of the United Sates...",
+//         fromWhom:"By Katherine Kato",
+//         cardImage:"https://images.unsplash.com/photo-1549092273-8b23dde8ac2b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+//     })
+//   return (
+//     <>
+//         <Card data={data} />
+//     </>
+//   )
+// }
+
