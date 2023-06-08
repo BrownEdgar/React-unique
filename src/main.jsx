d8eaa08a611ea5783a49a13b4986d94acebacd5c
ArmenDashysn <dashyan.armen@gmail.com>
2023-06-08 21:55:53 +0400

    my hm - 3

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from "./users/Armen/Axios/App"
+import App from "./App.jsx"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
