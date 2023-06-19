8267523eca8b4cebbe243f30bb69e2e63cb56ba0
ArmenDashysn <dashyan.armen@gmail.com>
2023-06-19 14:55:43 +0400

    aaa

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from "./users/Edgar/FORMS/App"
+import App from "./users/Armen/Forms/App2"
 import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
