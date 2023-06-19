3208ca499a4516fefd32a7cc9ba235768d471103
ArmenDashysn <dashyan.armen@gmail.com>
2023-06-19 22:24:00 +0400

    67

--- src/users/Armen/Crud/App.jsx
+++ src/users/Armen/Crud/App.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import { useReducer } from 'react'
+import reducer, { initialState } from './reducer'
+
+import './App.scss'
+
+import MainForm from './MainForm'
+import MainTable from './MainTable';
 
 export default function App() {
-  return (
-    <div className='App'>
-        <div className="foms"></div>
-        <div className="tables"></div>
-     
-    </div>
-  );
+	const [users, dispatch] = useReducer(reducer, initialState);
+
+	const addUser = (user) => { 
+		dispatch({
+			type: "ADD_USER",
+			payload: { user }
+		})
+	}
+
+	return (
+		<div className='App'>
+			<div className="App__forms">
+				<h1>Registration</h1>
+				<MainForm addUser={addUser}/>
+			</div>
+			<div className="buttons">
+				<button>by firstname</button>
+			</div>
+
+
+
+			<div className="App__tables">
+				<MainTable users={users}/>
+			</div>
+		</div>
+	)
 }
 
-export default App;
