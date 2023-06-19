27ffa3f328da7c55960cab5749990398f90237d4
ArmenDashysn <dashyan.armen@gmail.com>
2023-06-19 22:20:51 +0400

    67

---
+++ src/users/Armen/Crud/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className='App'>
+        <div className="foms"></div>
+        <div className="tables"></div>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/users/Armen/Crud/reduse.js
@@ -0,0 +1,8 @@
+const initialState =[
+    {
+        id: 1,
+        firstname: "Karen",
+        lastName: "Abrahamyan",
+        
+    }
+]
