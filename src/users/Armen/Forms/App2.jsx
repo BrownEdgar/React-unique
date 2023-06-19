9468bbafeac94958106221520d2b2eaa5776ae21
ArmenDashysn <dashyan.armen@gmail.com>
2023-06-19 16:26:12 +0400

    66

--- src/users/Armen/Forms/App2.jsx
+++ src/users/Armen/Forms/App2.jsx
@@ -1,40 +1,40 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
 const price = {
-    barselona: '680$',
-    'new-york': '1470$',
-    dublin: '340$',
-    yerevan: '150$'
+	barselona: '680$',
+	'new-york': '1470$',
+	dublin: '340$',
+	yerevan: '150$'
 }
+
 export default function App2() {
-    const [city, setCity] = useState('')
-    const handleChange = (e) => {
-        setCity(e.target.value)
-    }
-  return (
-    <div>
-        <h1>Select in React</h1>
-        <hr />
-        <form>
-            <select name="cities" id="cities" onChange={handleChange}>
-           {
-            Object.keys(price).map(elem => {
-                return (
-                   < option value={elem} key={elem}>
-                    {elem.replace(elem.at(0), elem.at(0).toUppercase())}
-                   </option>
-                )
-            })
-           }
-            </select>
-        </form>
+	const [city, setCity] = useState('');
+	const handleChange = (e) => {
+		setCity(e.target.value)
+	}
+	return (
+		<div>
+			<h1>Select in React</h1>
+			<hr />
+			<form>
+				<select name="cities" id="cities" onChange={handleChange}>
+					{
+						Object.keys(price).map(elem => {
+							return (
+								<option value={elem} key={elem}>
+									{elem.replace(elem.at(0), elem.at(0).toUpperCase())}
+								</option>
+							)
+						})
+					}
+				</select>
+			</form>
 
-        {city ? <h2>Ticket to 
-            <span style ={{ background: '#ee6352', padding: '3px' }}>
-                {city}
-            </span>
-             cost {price[city] } </h2> : null}
-
-    </div>
-  )
+			{city ? <h2>Ticket to
+				<span style={{ background: '#ee6352', padding: '3px' }}>
+					{city}
+				</span>
+				cost {price[city]}</h2> : null}
+		</div>
+	)
 }
