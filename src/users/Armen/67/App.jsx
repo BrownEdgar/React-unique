68762ebc1ccd7c6ca81c90c96f65f198afb2a0fa
ArmenDashysn <dashyan.armen@gmail.com>
2023-06-23 12:21:43 +0400

    my HM update

--- src/users/Armen/67/App.jsx
+++ src/users/Armen/67/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
         id: 2,
         category: "Cake",
         name: "Eskimo",
-        imges: 'https://cdn.lifehacker.ru/wp-content/uploads/2021/11/Depositphotos_68838977_XL_1637256577-scaled-e1637256646604-1280x640.jpg',
+        images: 'https://cdn.lifehacker.ru/wp-content/uploads/2021/11/Depositphotos_68838977_XL_1637256577-scaled-e1637256646604-1280x640.jpg',
         resept: "Крем для торта: 400 мл молока, 200 г сливочного масла, 150 г сахара, 2 яйца, 4 ст. л. какао-порошка, 2 ст. л. муки, 400 г вареного сгущенного молока.",
         description: "Насыщенные и хорошо пропитанные коржи, прослойка из воздушного сливочного крема и тонкий слой глазури — этот восхитительный торт вам захочется готовить на каждый праздник."
     },
@@ -24,7 +24,7 @@ export default function App() {
         id: 3,
         category: "Cake",
         name: "Napoleon",
-        imges: 'https://hips.hearstapps.com/hmg-prod/images/220713-delish-seo-01-napoleon-cake-horizontal-v5-081-eb-1658416382.jpg?crop=0.981xw:0.967xh;0.0187xw,0.0326xh&resize=1200:*',
+        images: 'https://hips.hearstapps.com/hmg-prod/images/220713-delish-seo-01-napoleon-cake-horizontal-v5-081-eb-1658416382.jpg?crop=0.981xw:0.967xh;0.0187xw,0.0326xh&resize=1200:*',
         resept: "Крем для торта: 400 мл молока, 200 г сливочного масла, 150 г сахара, 2 яйца, 4 ст. л. какао-порошка, 2 ст. л. муки, 400 г вареного сгущенного молока.",
         description: "Торт «Микадо» представляет собой сборку из множества тончайших сметанных коржей в сочетании с заварным шоколадным кремом и вареной сгущенкой. "
     },
@@ -32,7 +32,7 @@ export default function App() {
         id: 4,
         category: "Soft drinks",
         name: "Coca-Cola, Fanta, Sprite",
-        imges: 'https://indiaindia.com.cy/wp-content/uploads/2022/05/india-india-restaurant-nicosia-cyprus-soft-drinks22aa.jpg',
+        images: 'https://indiaindia.com.cy/wp-content/uploads/2022/05/india-india-restaurant-nicosia-cyprus-soft-drinks22aa.jpg',
         resept: " ",
         description: "Enjoy cool "
     },
@@ -40,7 +40,7 @@ export default function App() {
         id: 5,
         category: "Soft drinks",
         name: "Inclusive Mixed Drinks",
-        imges: 'https://i6b9r5w9.rocketcdn.me/wp-content/uploads/2020/03/mixed-drinks.jpg.webp',
+        images: 'https://i6b9r5w9.rocketcdn.me/wp-content/uploads/2020/03/mixed-drinks.jpg.webp',
         resept: " ",
         description: "Enjoy your time"
     },
@@ -48,7 +48,7 @@ export default function App() {
         id: 6,
         category: "Soft drinks",
         name: "Coffee",
-        imges: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNYn7EkLAfYtrJ7hAgVre38WOUV11uVKUNSA&usqp=CAU',
+        images: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNYn7EkLAfYtrJ7hAgVre38WOUV11uVKUNSA&usqp=CAU',
         resept: "Instant Coffee, Latte, Cappuccino, Americano, Espresso, Mocha",
         description: "Have a good time"
     },
@@ -56,7 +56,7 @@ export default function App() {
         id: 7,
         category: "Alcohols",
         name: "Brendy",
-        imges: 'https://www.shutterstock.com/image-photo/aged-brendy-cigar-old-distillery-260nw-1709782273.jpg',
+        images: 'https://www.shutterstock.com/image-photo/aged-brendy-cigar-old-distillery-260nw-1709782273.jpg',
         resept: "Ararat, Noy, Louis XIII, Hennessy",
         description: "Enjoy the history"
     },
@@ -64,7 +64,7 @@ export default function App() {
         id: 8,
         category: "Alcohols",
         name: "Beer",
-        imges: 'https://aspirantum.com/content/blog_post_images/blog_post_image_77_1572443204.jpg',
+        images: 'https://aspirantum.com/content/blog_post_images/blog_post_image_77_1572443204.jpg',
         resept: "Ararat, Kilikia, Dargett, Heineken, Bud, Stella Artois, Corona",
         description: "You can chose 0.33L, 0.5L and 1L"
     },
@@ -72,7 +72,7 @@ export default function App() {
         id: 9,
         category: "Alcohols",
         name: "Whisky",
-        imges: 'https://vinestore.ru/image/catalog/blog/burbon-wiski-scotch.jpg',
+        images: 'https://vinestore.ru/image/catalog/blog/burbon-wiski-scotch.jpg',
         resept: "Jameson, Jack Daniels, Chivas 12, Johnnie Walker (Red Label, Black Label, Blue Label, Duble Black, Green Lable, Gold Lable)",
         description: "Whisky or whiskey is a type of liquor made from fermented grain mash. Various grains (which may be malted) are used for different varieties, including barley, corn, rye, and wheat. Whisky is typically aged in wooden casks, which are typically made of charred white oak. Uncharred white oak casks previously used for the aging of port, rum or sherry are also sometimes used."
     },
@@ -80,7 +80,7 @@ export default function App() {
         id: 10,
         category: "Main Dishes",
         name: "Butter Chicken",
-        imges: 'https://picturetherecipe.com/wp-content/uploads/2020/04/PictureTheRecipe-Butter-Chicken.jpg',
+        images: 'https://picturetherecipe.com/wp-content/uploads/2020/04/PictureTheRecipe-Butter-Chicken.jpg',
         resept: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
         description: "There’s about a bazillion versions of this very popular Indian dish, which is why I haven’t gotten around with sharing my own version on here before. Also it’s one of those recipes that I usually make from memory and had never actually written down to be able to share even though almost every time I’ve made it for someone they’ve asked for the recipe."
     },
@@ -88,7 +88,7 @@ export default function App() {
         id: 11,
         category: "Main Dishes",
         name: "Palak Paneer",
-        imges: 'https://picturetherecipe.com/wp-content/uploads/2019/10/Palak-Paneer-by-PictureTheRecipe.jpg',
+        images: 'https://picturetherecipe.com/wp-content/uploads/2019/10/Palak-Paneer-by-PictureTheRecipe.jpg',
         resept: "So here’s my version of Palak Paneer a classic north Indian dish of cottage cheese cubes in a spinach based sauce. If you don’t have access to Indian Paneer, you can try tofu or even mini mozzarella balls, just skip the pan frying if you are using mozzarella.",
         description: "Indian cottage cheese over a vibrant green spiced spinach sauce and finished with a splash of cashew cream."
     },
@@ -96,10 +96,26 @@ export default function App() {
         id: 12,
         category: "Main Dishes",
         name: "Grilled Tomahawk Steak",
-        imges: 'https://www.allrecipes.com/thmb/KFsMHU1dhZHfrYGgaORJ98GvnHk=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/7508957_Grilled-Tomahawk-Steak_Ashley-Elizabeth_4x3-5b17eab73d154d6394829d47d6ff3365.jpg',
+        images: 'https://www.allrecipes.com/thmb/KFsMHU1dhZHfrYGgaORJ98GvnHk=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/7508957_Grilled-Tomahawk-Steak_Ashley-Elizabeth_4x3-5b17eab73d154d6394829d47d6ff3365.jpg',
         resept: "To make this an easy meal, I like to serve the steak on a platter of baked rice, and a chopped wedge salad with Bob's Blue Cheese Dressing! You can make the baked rice the day before, then reheat it with your steak. And the wedge salad can be assembled in the morning, and kept in the fridge until you're ready to serve.",
         description: "This grilled tomahawk steak recipe is a showstopper, ideal for sharing with friends and family. This reverse-seared steak is cooked to perfection with an unctuous compound butter filled with shallots, garlic, and chives. I like to use a Bluetooth-enabled meat thermometer to help me achieve the perfect medium-rare internal temperature."
     },
+    {
+			id: 13,
+      category: "Cake",
+			name: "Торт",
+			images: 'https://tart-tort.ru/uploads/product/100/107/123-3_2021-08-25_15-48-41.JPG',
+			resept: "Крем для торта: 400 мл молока, 200 г сливочного масла, 150 г сахара, 2 яйца, 4 ст. л. какао-порошка, 2 ст. л. муки, 400 г вареного сгущенного молока.",
+			description: "Торт «Микадо» представляет собой сборку из множества тончайших сметанных коржей в сочетании с заварным шоколадным кремом и вареной сгущенкой. "
+		},
+    {
+			id: 14,
+      category: "Cake",
+			name: "Пьяная вишня",
+			images: 'https://www.videoculinary.ru/wp-content/uploads/2012/06/piyanaya-vishna_14-300x169.jpg',
+			resept: "Тесто: Яйца – 9 штук, Сахар – 180 грамм, Мука для выпечки (или обычная мука и 0,5 чайной ложки разрыхлителя) – 130 грамм, Какао – 80 грамм. Крем: Масло сливочное – 300 грамм, Сгущённое молоко – 400 грамм. Начинка: Вишня без косточек – 2,5 стакана, Водка/коньяк/ром – 0,5 стакана. Шоколадная глазурь: Сливки – 180 грамм, Шоколад горький - 150 грамм, Масло сливочное – 25 грамм, Сахар – 25 грамм.",
+			description: "За одни сутки до приготовления торта Пьяная вишня кладём в мисочку 2,5 стакана вишни без косточек, добавляем 2 столовые ложки сахара, слегка перемешиваем и заливаем 0,5 стакана водки, коньяка или рома. Накрываем и оставляем при комнатной температуре. (Можно использовать консервированную вишню без косточек, в этом случае сахар добавлять не надо)."
+		},
 ]);
 
 const [selectedCategory, setSelectedCategory] = useState("");
@@ -140,7 +156,7 @@ return (
           .map((cake) => (
             <div className="App__cake" key={cake.id}>
               <div className="App__imageBox">
-                <img src={cake.imges} alt="" />
+                <img src={cake.images} alt="" />
               </div>
               <div className="App__content">
                 <h2>{cake.name}</h2>
