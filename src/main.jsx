3904824c3d25d3ac88ff694ba28f0c9daa5c2160
Sash-1 <sasha.aristakesyan@mail.ru>
2023-06-26 18:36:20 +0400

    no change

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from "./users/Armen/68/App"
+import App from "./users/Sash/Movies/App"
 import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
