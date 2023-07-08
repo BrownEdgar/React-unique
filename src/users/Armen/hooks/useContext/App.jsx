9f06c1da26764ff4f47e82787ee392d94205b6d5
ArmenDashsan <you@example.com>
2023-07-08 16:21:59 +0400

    lesson75

--- src/users/Armen/hooks/useContext/App.jsx
+++ src/users/Armen/hooks/useContext/App.jsx
@@ -1,24 +1,41 @@
 import  { useState,createContext } from 'react';
+import Items from './Items'
 
-export const MyContext = createContext('barev')
+
+import './App.scss'
+
+export const MyContext = createContext([])
 
 export default function App() {
 	const [data, setData] = useState([
 		{
 			id:1,
-			image: ''
-		}
+			image: '../../../../../public/images/Rectangle 33.jpg',
+			description: 'Завершили строительство и 	организовали продажу муниципального объекта 		недвижимости дороже первоначальной стоимости 		на 22 млн. рублей.'
+		},
+		{
+			id:2,
+			image: '../../../../../public/images/Rectangle 34.jpg',
+			description: 'Завершили строительство и 	организовали продажу муниципального объекта 		недвижимости дороже первоначальной стоимости 		на 22 млн. рублей.'
+		},
+		{
+			id:3,
+			image: '../../../../../public/images/Rectangle 35.jpg',
+			description: 'Завершили строительство и 	организовали продажу муниципального объекта 		недвижимости дороже первоначальной стоимости 		на 22 млн. рублей.'
+		},
+		{
+			id:4,
+			image: '../../../../../public/images/Rectangle 36.jpg',
+			description: 'Завершили строительство и 	организовали продажу муниципального объекта 		недвижимости дороже первоначальной стоимости 		на 22 млн. рублей.'
+		},
 	])
 	
 	return (
-		<div>
-			<h1>APP COMPONENT</h1>
-			<MyContext.Provider value={{
-				arr:value,
-				total,
-			}}>
-				<A />
+		<div className='App'>
+			<MyContext.Provider value={data}>
+				<Items />
 			</MyContext.Provider>
+			
 		</div>
 	)
 }
---
+++ src/users/Armen/hooks/useContext/Item.jsx
@@ -0,0 +1,19 @@
+export default function Item({item}) {
+  return (
+    <div className='item'>
+        <div className="item__image">
+            <img src={item.image} />
+        </div>
+        <div className="item__content">
+            <p>{item.description}</p>
+            <div className="item__icon">
+                <span>
+                    <i className="fa-solid fa-arrow-right"></i>
+                    </span>
+            </div>
+        </div>
+        
+    </div>
+    
+  )
+}
