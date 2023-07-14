2d30795571454bc476d904cc7493a236343dbaf2
BrownEdgar <nikogosjanedgar@gmail.com>
2023-07-14 20:46:56 +0400

    change slider view

--- src/Slick-Slider/App.jsx
+++ src/Slick-Slider/App.jsx
@@ -1,6 +1,18 @@
 import Slider from "react-slick";
+import { useState } from 'react';
+
 import "./App.css"
+
 export default function App() {
+	const [data, setdata] = useState(
+		{
+			1: 'https://images.unsplash.com/photo-1661956603025-8310b2e3036d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+			2: 'https://plus.unsplash.com/premium_photo-1669559809539-acc3b2e05f8c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80',
+			3:'https://images.unsplash.com/photo-1689176748178-b7989e722873?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80',
+			4:'https://images.unsplash.com/photo-1689126494042-39f69fa4c8c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
+		}
+	)
+	
   const settings = {
     dots: true,
     infinite: true,
@@ -9,42 +21,19 @@ export default function App() {
     slidesToScroll: 1,
     autoplay: true,
     autospeed: 2000,
-  };
+  }
   return (
     <div className="App">
       <Slider {...settings}>
-        <div>
-          <div className="img-box">
-          <img src="https://images.unsplash.com/photo-1661956603025-8310b2e3036d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="" />
-          </div>
-        </div>
-        <div>
-          <img src="https://plus.unsplash.com/premium_photo-1669559809539-acc3b2e05f8c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80" alt="" />
-        </div>
-        <div>
-          <img src="https://images.unsplash.com/photo-1689176748178-b7989e722873?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80" alt="" />
-        </div>
-        <div>
-          <img src="https://images.unsplash.com/photo-1689126494042-39f69fa4c8c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="" />
-        </div>
-        <div>
-          <img src="https://images.unsplash.com/photo-1682687982204-f1a77dcc3067?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80" alt="" />
-        </div>
-        <div>
-          <img src="https://images.unsplash.com/photo-1689085781839-2e1ff15cb9fe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="" />
-        </div>
-        <div>
-          <img src="https://images.unsplash.com/photo-1688980034676-7e8ee518e75a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80" alt="" />
-        </div>
-        <div>
-          <img src="https://images.unsplash.com/photo-1688934728398-99e6801f3e4b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80" alt="" />
-        </div>
-        <div>
-          <img src="https://images.unsplash.com/photo-1689089376904-4755ada5058a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80" alt="" />
-        </div>
-        <div>
-          <img src="https://images.unsplash.com/photo-1689126437563-f5d936a06713?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80" alt="" />
-        </div>
+				{
+					Object.values(data).map((elem, index) => {
+						return (
+							<div key={index}>
+								<img src={elem} alt={`pht-${index}`} />
+							</div>
+						)
+					})
+				}
       </Slider>
     </div>
   )
