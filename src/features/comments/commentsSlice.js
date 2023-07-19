9d51fc11ac7ec85857f37a3169ea58825ced899f
ArmenDashsan <you@example.com>
2023-07-19 20:54:46 +0400

    80

---
+++ src/features/comments/commentsSlice.js
@@ -0,0 +1,14 @@
+import { createSlice} from "@reduxjs/toolkit";
+
+const creatSlice = {
+    data: [],
+    status: 'idle'
+}
+
+const commentsSlice = creatSlice({
+    name: 'comments',
+    initialState,
+    redusers: {}
+})
+
+export default commentsSlice.reduser;
---
+++ src/features/index.js
@@ -0,0 +1,1 @@
+export {default as commentReducer} from './todos/todosSlice'
