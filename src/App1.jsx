b22c0b5e1d83a789a514cf7638ca0874fc80afc8
ArmenDashsan <you@example.com>
2023-07-21 19:48:36 +0400

    updating HM lesson 80

--- src/App1.jsx
+++ src/App1.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Slider from 'react-slick';
 import Users from './components/Users';
 import Posts from './components/Posts';
 import Todos from './components/Todos';
@@ -6,12 +7,32 @@ import Albums from './components/Albums';
 import './App.css';
 
 export default function App() {
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '0',
+  };
+
   return (
     <div>
-      <Users />
-      <Posts />
-      <Todos />
-      <Albums />
+      <Slider {...sliderSettings}>
+        <div>
+          <Users />
+        </div>
+        <div>
+          <Posts />
+        </div>
+        <div>
+          <Todos />
+        </div>
+        <div>
+          <Albums />
+        </div>
+      </Slider>
     </div>
   );
 }
